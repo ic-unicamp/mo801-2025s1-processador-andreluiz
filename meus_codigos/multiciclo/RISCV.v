df116a7ac4ce19ad942fc8c06baccc36bebcbb5f
Andre Luiz Vasconcelos Ferreira <andreluiz123@gmail.com>
2025-03-23 12:12:38 -0300

    Instancia multiplexador do srcA, oldPC e PC

--- meus_codigos/multiciclo/RISCV.v
+++ meus_codigos/multiciclo/RISCV.v
@@ -7,13 +7,14 @@ module RISCV;
     reg [31:0] WD3;       // Dado a ser escrito no registrador
     wire [31:0] RD1, RD2; // Dados lidos dos registradores
     reg [24:0] immValue;
-    wire [31:0] result_multiplexRD2_immExt;
+    wire [31:0] result_multiplex_PC_oldPC_RD1, result_multiplex_RD2_immExt_4;
 
     reg [31:0] srcA, srcB; // Operandos da ALU
-    reg [1:0] ALUControl, ALUSrcB;  // Controle da operação da ALU
+    reg [1:0] ALUControl, ALUSrcA, ALUSrcB;  // Controle da operação da ALU
     wire Zero;             // Flag Zero da ALU
     wire [31:0] ALUResult; // Resultado da ALU
     wire [31:0] immExt;
+    reg [31:0] PC, oldPC;
 
     // Instância do RegisterFile
     RegisterFile regfile_inst (
@@ -36,22 +37,32 @@ module RISCV;
         .immExt(immExt)
     );
 
-    Multiplex multiplex_RD2_immExt (
+    Multiplex multiplex_RD2_immExt_4 (
     	.clk(clk),
     	.reset(reset),
     	.ALUSrc(ALUSrcB),
     	.opt1(RD2),
     	.opt2(immExt),
     	.opt3(32'd4),
-    	.result(result_multiplexRD2_immExt)
+    	.result(result_multiplex_RD2_immExt_4)
+    );
+    
+    Multiplex multiplex_PC_oldPC_RD1 (
+    	.clk(clk),
+    	.reset(reset),
+    	.ALUSrc(ALUSrcA),
+    	.opt1(PC),
+    	.opt2(oldPC),
+    	.opt3(RD1),
+    	.result(result_multiplex_PC_oldPC_RD1)
     );
 
     // Instância do ALU
     ALU alu_inst (
         .clk(clk),
         .reset(reset),
-        .srcA(RD1),
-        .srcB(result_multiplexRD2_immExt),
+        .srcA(result_multiplex_PC_oldPC_RD1),
+        .srcB(result_multiplex_RD2_immExt_4),
         .ALUControl(ALUControl),
         .Zero(Zero),
         .ALUResult(ALUResult)
@@ -79,10 +90,13 @@ module RISCV;
 	    A1 = 5'd2;        // Defina A1 para acessar o registrador escrito
 	    A2 = 5'd2;
 	    immValue = 24'd15;
+	    PC = 32'd5;
+	    oldPC = 32'd4;
 	    #10;
 	    clk = 3;
 	    ALUControl = 2'b10;
 	    ALUSrcB = 2'b10;
+	    ALUSrcA = 2'b01;
   
 	    // Passo 4: Verificar se o valor lido é igual a 30
 	    $display("Valor em Registers[A1]: %d; Registers[A2]: %d; immExt: %d; ALUResult: %d", RD1, RD2, immExt, ALUResult);
