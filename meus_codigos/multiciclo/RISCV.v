9b525260f31057b40c7fb34def06760a4446413d
Andre Luiz Vasconcelos Ferreira <andreluiz123@gmail.com>
2025-03-25 21:44:27 -0300

    Refatora RISCV.v, renomeando variáveis e comentando seus significados

--- meus_codigos/multiciclo/RISCV.v
+++ meus_codigos/multiciclo/RISCV.v
@@ -1,103 +1,156 @@
 module RISCV;
 
-// Sinais
-reg clk, reset, IRWrite;
-reg[1:0] immSrc;
-reg [31:0] PC, RD;
+//Sinais
+reg clk, reset;
+
+//Inputs do Controller
+wire [2:0] func3; // 3 bits
+wire [6:0] opcode, func7; // 7 bits
+
+//Sinais do Controller
+reg IRWrite, MemWrite, AdrSrc, PCWrite, RegWrite; //1 bit
+reg [1:0] ResultSrc, ALUSrcB, ALUSrcA, ImmSrc; //2 bit
+reg [2:0] ALUControl; 
+
+//Inputs do ProgramCounter
+reg [31:0] PCNext;
+
+//Outputs do ProgramCounter
+wire [31:0] PC;
+
+//Inputs do Memory
+wire [31:0] address;
+
+//Outputs do Memory
+wire [31:0] ReadData; //Também é input do fetch data
+
+//Outputs do FetchData (além dos já declarados func3, func7 e opcode)
+wire [4:0] Rs1, Rs2, Rd; //Também é input do register file
+wire [24:0] extend; //Também é input do extend
 wire [31:0] OldPC;
 
-reg [31:0] WD3;
-reg WE3;
+//Outputs do RegisterFile
+wire [31:0] RD1, RD2; //O RD2 também é Input do memory, fazendo o papel de Write Data
 
-//Fios entre o InstructionFetch e o RegFile/Extend
-wire [4:0] Instruction_rs1, Instruction_rs2, Instruction_rd;
-wire [24:0] Instruction_extend;
-wire [6:0] Instruction_op;
-wire [2:0] Instruction_func3;
-wire [6:0] Instruction_func7;
+//Outputs do Extend
+wire [31:0] ImmExt;
 
-//Fios de saída do RegFile
-wire [31:0] RD1, RD2;
+//Output da ALU
+wire [31:0] ALUResult, ALUOut;
 
-//Fios de saída do Extend
-wire [11:0] immExt_l;
+//Output dos multiplexadores
+//Multiplexador ALU - ALUResult(00), ReadData(01), ALUOut(10)
+wire [31:0] multiplex_alu;
 
-//Fios que saem da ALU
-wire [31:0] ALUResult;
+//Multiplexador SrcA - PC(00), OldPC(01), RD1(10)
+wire [31:0] multiplex_srcA;
 
-InstructionFetch inst_fetch(
+//Multiplexador SrcB - RD2(00), ImmExt(01), 4(10)
+wire [31:0] multiplex_srcB;
+
+//Multiplexador Adr - PC(0), address(1)
+wire [31:0] multiplex_adr;
+
+Multiplex_address Mult_adr(
+	.PC(PC),
+	.ALUResult(multiplex_alu), 
+	.AdrSrc(AdrSrc), 
+	.address(multiplex_adr)
+);
+
+memory Mem(
+  .address(address),
+  .data_in(RD2),
+  .data_out(ReadData),
+  .we(MemWrite)
+);
+
+InstructionFetch Inst_fetch(
 	.clk(clk), 
 	.reset(reset), 
 	.IRWrite(IRWrite), 
 	.PC(PC), 
-	.RD(RD), 
+	.RD(ReadData), 
 	.OldPC(OldPC), 
-	.Instruction_rs1(Instruction_rs1), 
-	.Instruction_rs2(Instruction_rs2), 
-	.Instruction_rd(Instruction_rd), 
-	.Instruction_extend(Instruction_extend), 
-	.Instruction_op(Instruction_op), 
-	.Instruction_func3(Instruction_func3), 
-	.Instruction_func7(Instruction_func7)
+	.Instruction_rs1(Rs1), 
+	.Instruction_rs2(Rs2), 
+	.Instruction_rd(Rd), 
+	.Instruction_extend(extend), 
+	.Instruction_op(opcode), 
+	.Instruction_func3(func3), 
+	.Instruction_func7(func7)
 );
 
-RegisterFile reg_file(
+RegisterFile Reg_file(
 	.clk(clk),
 	.reset(reset),
-	.A1(Instruction_rs1),
-	.A2(Instruction_rs2),
-	.A3(Instruction_rd),
-	.WD3(WD3),
-	.WE3(WE3),
+	.A1(Rs1),
+	.A2(Rs2),
+	.A3(Rd),
+	.WD3(multiplex_alu),
+	.WE3(RegWrite),
 	.RD1(RD1),
 	.RD2(RD2)
 );
 
-Extend extend(
+Extend Ext(
 	.clk(clk),
 	.reset(reset), 
-	.immValue(Instruction_extend), 
-	.immSrc(immSrc), 
-	.immExt_l(immExt_l)
+	.immValue(extend), 
+	.immSrc(ImmSrc), 
+	.immExt_l(ImmExt)
 );
 
-ALU alu(
+Multiplex Multiplex_instance_SrcA (
+	.clk(clk),
+	.reset(reset),
+	.ALUSrc(ALUSrcA),
+	.opt1(PC),
+	.opt2(OldPC),
+	.opt3(RD1),
+	.result(multiplex_srcA)
+);
+
+Multiplex Multiplex_instance_SrcB (
+	.clk(clk),
+	.reset(reset),
+	.ALUSrc(ALUSrcB),
+	.opt1(RD2),
+	.opt2(ImmExt),
+	.opt3(32'd4),
+	.result(multiplex_srcB)
+);
+
+ALU Alu(
 	.clk(clk),
  	.reset(reset), 
- 	.srcA(RD1), 
- 	.srcB(immExt_l), 
- 	.ALUControl(2'b10), 
+ 	.srcA(multiplex_srcA), 
+ 	.srcB(multiplex_srcB), 
+ 	.ALUControl(ALUControl), 
  	.Zero(0), 
  	.ALUResult(ALUResult)
 );
 
+
+Multiplex Multiplex_instance_ALU(
+	.clk(clk),
+	.reset(reset),
+	.ALUSrc(ResultSrc),
+	.opt1(ALUResult),
+	.opt2(ReadData),
+	.opt3(ALUOut),
+	.result(multiplex_alu)
+);
+
+
 // Gerador de clock
 initial begin
 clk = 0;
-#10;
 end
 
 // Simulação inicial
 initial begin
-     clk = 1;
-     reset = 1;
-     PC = 1'd1;
-     RD = 32'b00000000010000010010000100000011;
-     #10;	 
-     clk = 2;     
-     reset = 0;
-     #10;
-     clk = 3;
-     immSrc = 2'b00;
-     #10;
-     clk = 4;
-     immSrc = 2'b00;
 
-    $display("RD=%b",RD);
-    $display("Instruction_rs1 = %b, Instruction_rs2= %b, Instruction_rd= %b, Instruction_extend= %b, Instruction_op= %b, Instruction_func3= %b, Instruction_func7= %b", Instruction_rs1, Instruction_rs2, Instruction_rd, Instruction_extend, Instruction_op, Instruction_func3, Instruction_func7);
-    $display("RD1 = %b, RD2= %b", RD1, RD2);
-    $display("immExt_l= %b", immExt_l);
-    $display("ALUResult = %d",ALUResult);
 end
 
 
